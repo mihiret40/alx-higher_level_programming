c2fbef336db20bbbe81541d7383d5b08b7e27475
mihiret40 <mihiret4027@gmail.com>
2022-10-12 12:17:23 +0000

    task 0 upto 9

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""File defines a square"""
+
+
+
+
+
+class Square:
+
+    """A class Square that defines a square"""
+
+    def __init__(self, size=None):
+
+        """Initializes private attribute size """
+
+        self.__size = size
