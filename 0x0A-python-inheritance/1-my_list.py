be471576e3270e0023038b312015cc96d20eb299
mihiret40 <mihiret4027@gmail.com>
2022-10-24 11:57:44 +0000

    task 0 upto 12

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,25 @@
+ns the MyList class
+
+"""
+
+
+
+
+
+class MyList(list):
+
+    """a subclass of list"""
+
+    def __init__(self):
+
+        """initializes the object"""
+
+        super().__init__()
+
+
+
+    def print_sorted(self):
+
+        """prints the sorted list"""
+
+        print(sorted(self))
